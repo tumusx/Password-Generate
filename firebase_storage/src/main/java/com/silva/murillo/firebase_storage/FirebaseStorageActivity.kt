e77f6f9c3eb404a8a308e2f63e77255f513465a3
Murillo Alves <tumusx1@gmail.com>
2022-05-19 23:41:07 -0300

    ajustes

--- firebase_storage/src/main/java/com/silva/murillo/firebase_storage/FirebaseStorageActivity.kt
+++ firebase_storage/src/main/java/com/silva/murillo/firebase_storage/FirebaseStorageActivity.kt
@@ -1,6 +1,5 @@
 package com.silva.murillo.firebase_storage
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,55 +10,47 @@ import androidx.appcompat.widget.AppCompatButton
 import com.bumptech.glide.Glide
 import com.bumptech.glide.annotation.GlideModule
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.google.firebase.storage.ktx.storage
-import java.io.File
 
 @GlideModule
 class FirebaseStorageActivity : AppCompatActivity() {
     private lateinit var txtDownload: TextView
-    private lateinit var appButonDownload: AppCompatButton
+    private lateinit var appButtonDownload: AppCompatButton
     private lateinit var txtUpload: TextView
-    private lateinit var appButonUpload: AppCompatButton
+    private lateinit var appButtonUpload: AppCompatButton
     private lateinit var imgFirebase: ImageView
-    val storageReference = Firebase.storage.reference
+    private val storageReference = Firebase.storage.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase_storage)
         txtDownload = findViewById(R.id.txtDownload)
         txtUpload = findViewById(R.id.txtUpload)
-        appButonDownload = findViewById(R.id.appButonDownload)
+        appButtonDownload = findViewById(R.id.appButonDownload)
         imgFirebase = findViewById(R.id.imgFirebase)
-        appButonUpload = findViewById(R.id.appButonUpload)
+        appButtonUpload = findViewById(R.id.appButonUpload)
         onConfigureDownloadStorage()
     }
 
     private fun onConfigureDownloadStorage() {
-        var showResponseDownload = "Sucesso ao realizar o download."
+        var showResponseDownload = "Success ao realize o download."
         val storageRef = storageReference
-        appButonDownload.apply {
+        appButtonDownload.apply {
             setOnClickListener {
                 val storageDownload: StorageReference =
-                    storageRef.child("storage").child("ticketlog.png")
+                    storageRef.child("storage").child("file.pdf")
                 storageDownload.downloadUrl.addOnSuccessListener {it->
-                    Log.d("string", "${it}")
+                    Log.d("string", "$it")
                     Toast.makeText(context, showResponseDownload, Toast.LENGTH_SHORT).show()
                     Glide.with(this).asDrawable()
                         .load(storageRef).into(imgFirebase)
                 }.addOnFailureListener{
-                    showResponseDownload = "Erro ao realizar o download"
+                    showResponseDownload = "Error ao realize o download"
                     Toast.makeText(context, showResponseDownload, Toast.LENGTH_SHORT).show()
                 }
             }
-
         }
-
-    }
-
-    private fun onConfigureUploadStorage(){
-
     }
 
 }
